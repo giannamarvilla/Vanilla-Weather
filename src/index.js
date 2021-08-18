cdf73773acf59cd9486cf0e6f4ecbbfccf667e8d
giannamarvilla <giannamarvilla@gmail.com>
2021-08-18 22:00:47 +0900

    Adding Active Links

--- src/index.js
+++ src/index.js
@@ -115,6 +115,8 @@ function handleSubmit(event) {
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
